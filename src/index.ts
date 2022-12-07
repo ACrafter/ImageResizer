22f5cc9633a4a43077f96efd6342803512eb31e8
ACrafter <ahmed2002yasser.ay@gmail.com>
2022-12-07 08:24:55 +0200

    Added Basic UI

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import Express from "express";
 import router from "./router/index";
+import path from "path";
 
 const app = Express();
 const port = 3000;
 
 app.use("/", router);
+app.use(Express.static(path.resolve("frontend")));
+app.use(Express.urlencoded({ extended: true }));
+
 
 app.listen(port, () => {
   console.log(`Server Running on Port ${port}`);
